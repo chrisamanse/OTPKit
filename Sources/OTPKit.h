c79ed3774c8cc24c0cbec66f37d7d2d3f67b5e26
Chris Amanse <chris@chrisamanse.xyz>
2016-09-03 12:23:56 +0800

    Initial commit

---
+++ Sources/OTPKit.h
@@ -0,0 +1,19 @@
+//
+//  OTPKit.h
+//  OTPKit
+//
+//  Created by Chris Amanse on 03/09/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OTPKit.
+FOUNDATION_EXPORT double OTPKitVersionNumber;
+
+//! Project version string for OTPKit.
+FOUNDATION_EXPORT const unsigned char OTPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTPKit/PublicHeader.h>
+
+
