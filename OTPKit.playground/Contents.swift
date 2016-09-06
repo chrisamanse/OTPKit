a18e70d15dc0e6bf3d5e4f34f6019df742d0b8b9
Chris Amanse <chris@chrisamanse.xyz>
2016-09-06 18:33:00 +0800

    Remove guard statement for getting data of secret

--- OTPKit.playground/Contents.swift
+++ OTPKit.playground/Contents.swift
@@ -7,9 +7,7 @@ var str = "Hello, playground"
 
 let base32String = "V3ZMBGAETLLSXRJZ6QZD42Z33O3DK3R7"
 
-guard let secret = try? Base32.decode(base32String) else {
-    abort()
-}
+let secret = try! Base32.decode(base32String)
 
 let passwordGenerator = TOTPGenerator(key: secret, period: 30, digits: 6, hashFunction: .sha1)
 
